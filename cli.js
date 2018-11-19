d2b304548edcc357940cf10f11dfb9c69d0d0fb6
Francis Brito <fr.br94@gmail.com>
2018-11-19 12:54:53 -0400

    Update CLI to support listing style names

--- cli.js
+++ cli.js
@@ -1,26 +1,30 @@
 #!/usr/bin/env node
 const path = require('path');
+const yargs = require('yargs');
+const { prop } = require('ramda');
 
 const listDocxStyle = require('.');
 
-const usage = '$ docx-list-style <file-path>';
+const usage = '$0 <file-path> [options]';
 const helpText = 'Prints a list of the styles being used by a Word (.docx) file.';
-const man = `
-${usage}
-${helpText}
-`;
-const input = process.argv[ 2 ];
-
-if (!input) {
-  console.log(man);
-  process.exit(0);
-}
-
+const argv = yargs
+  .usage(usage, helpText)
+  .scriptName('docx-list-style')
+  .demandCommand(1)
+  .help('h')
+  .alias('h', 'help')
+  .boolean('n')
+  .alias('n', 'names')
+  .describe('n', 'Lists style names instead of IDs')
+  .argv;
+const input = argv['file-path'];
+const shouldListNames = argv.n;
 const filePath = path.isAbsolute(input) ? input : path.resolve(process.cwd(), input);
+const convertStyleToString = shouldListNames ? prop('name') : prop('styleId');
 
 listDocxStyle
   .fromFilePath(filePath)
-  .then(entries => entries.map(({ styleId }) => styleId).sort())
+  .then(entries => entries.map(convertStyleToString).sort())
   .then(styles => styles.forEach(s => console.log(s)))
   .catch(e => {
     console.error(e.message);
